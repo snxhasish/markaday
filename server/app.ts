0a3757bd4c753ddf87c4ecf9bcafd1ac4d1e7e35
snehasishcodes <snehasishcodes@gmail.com>
2026-01-01 15:07:53 +0530

    test on prod #1

--- server/app.ts
+++ server/app.ts
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: process.env.ORIGIN_URL,
+        origin: "https://markaday.vercel.app",
         credentials: true,
     })
 );
